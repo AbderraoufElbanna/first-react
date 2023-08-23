e70879302d993cab291e4ca2fe4ea0855219903d
Gomyown <raoufelbenna@gmail.com>
2023-08-23 20:56:19 +0100

    I added text to the README file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [Counter,setCounter] = useState (0);
+  const increase = ()=> {
+    setCounter(Counter+1)
+  }
+  const decrease = ()=> {
+    Counter > 0 ? setCounter(Counter - 1):alert ("Te3ba La3bed")
+    }
+
+    const reset = ()=> {setCounter(0)}
+
+
+
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello State </h1>
+      <h3>Value : {Counter}</h3>
+      <br/>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <br/>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
